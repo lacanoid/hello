6e6ac074b40195ea6f94c5fdc29268595dcff60d
Žiga Kranjec <ziga@ljudmila.org>
2019-10-21 20:38:39 +0200

    Makefile

--- server/index.js
+++ server/index.js
@@ -8,12 +8,16 @@ const parceloptions = {
 const parcelentry = Path.resolve(__dirname, "../client/index.html")
 const bundler = new Parcel(parcelentry, parceloptions)
 const staticdir = Path.resolve(__dirname, "../static")
+const builddir = Path.resolve(__dirname, "../dist")
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use('/static',express.static(staticdir))
 app.use('/api',require('./api.js'))
-app.use(bundler.middleware())
+
+if(process.env.NODE_ENV==="production") app.use('/',express.static(builddir))
+else app.use(bundler.middleware());
+
 
 app.listen(port, ()=>{ console.log('Listening on port ',port)})
